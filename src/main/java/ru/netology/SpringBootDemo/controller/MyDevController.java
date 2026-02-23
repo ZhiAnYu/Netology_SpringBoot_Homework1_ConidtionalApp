5bd7db210a123a096019b38623182df547ae332a
Zhi An <zhidkovaayu@gmail.com>
2026-02-23 14:08:45 +0400

    Add validation

--- src/main/java/ru/netology/SpringBootDemo/controller/MyDevController.java
+++ src/main/java/ru/netology/SpringBootDemo/controller/MyDevController.java
@@ -1,6 +1,7 @@
 package ru.netology.SpringBootDemo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class MyDevController {
 //    }
 
     @PostMapping("/hello")
-    private String hello(@RequestBody Person guest) {
+    private String hello(@Validated @RequestBody Person guest) {
         return String.format("Hello from %s to name %s age %d!", from,
                 guest.getName(),
                 guest.getAge());
--- src/main/java/ru/netology/SpringBootDemo/domain/Person.java
+++ src/main/java/ru/netology/SpringBootDemo/domain/Person.java
@@ -1,9 +1,18 @@
 package ru.netology.SpringBootDemo.domain;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Person {
+    @NotBlank
+    @Size(min=2, max=20)
     private String name;
+    @Min(10)
+    @Max(99)
     private int age;
 
     public Person() {
